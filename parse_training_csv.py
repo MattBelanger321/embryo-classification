9d287c954f1e7fc56f2cb75209bc72dde6aae6ea
Matthew Belanger <matt_belanger1@outlook.com>
2024-10-23 12:56:12 -0400

    added normalization that approximately makes all scans the same contrast

--- parse_training_csv.py
+++ parse_training_csv.py
@@ -86,6 +86,17 @@ def parse_gi_tract_training_data(csv_file_path='./data/train.csv'):
 				i = 0
 			i += 1
                
+# Assumes image is 16bit
+#sets min to 0, max to 1. All data is on the range [0,1] and uses the whole range
+def normalize(matrix_16bit):
+    matrix_16bit = matrix_16bit - matrix_16bit.min()
+    max = matrix_16bit.max()
+    if max == 0:
+        max = 1
+    matrix_16bit = matrix_16bit / max
+    return matrix_16bit
+
+               
 def load_data(csv_file_path='./data/train.csv', width=256, height=256):
     sample_counter = 1
     # Parse the segmentation masks and original files
@@ -102,8 +113,9 @@ def load_data(csv_file_path='./data/train.csv', width=256, height=256):
         labels_array = np.transpose(labels_array, (1, 2, 0))
         labels_array = cv2.resize(labels_array, (width,height), interpolation=cv2.INTER_LINEAR)
 
-        input_as_matrix = cv2.imread(original_file_path, cv2.IMREAD_ANYDEPTH)/65535.0 # re-scale to (0,1) (data is 16bit)
+        input_as_matrix = cv2.imread(original_file_path, cv2.IMREAD_ANYDEPTH)
         input_as_matrix = cv2.resize(input_as_matrix,(width,height), interpolation=cv2.INTER_LINEAR)
+        input_as_matrix = normalize(input_as_matrix)
         input_as_matrix = np.asarray(input_as_matrix)
 
         if sample_counter % 500 == 0:
