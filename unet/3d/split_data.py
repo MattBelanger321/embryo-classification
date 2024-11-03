b5639809661a20e42e1d04bed6231ac5103232ab
Matthew Belanger <matt_belanger1@outlook.com>
2024-11-03 17:10:51 -0500

    implemented directory contatenation function

--- unet/3d/split_data.py
+++ unet/3d/split_data.py
@@ -3,6 +3,8 @@
 import glob
 import random
 
+import os
+
 def count_cases(input_dir):
 	return 267
 
@@ -24,6 +26,23 @@ def split_filenames(paired_filenames, val_ratio=0.1, test_ratio=0.1):
 
 	return (zip(*train_pairs), zip(*val_pairs), zip(*test_pairs))
 
+def concatenate_directory_entries(input_directories, label_directories):
+    def get_files_from_directories(directories):
+        file_set = set()  # Use a set to avoid duplicates
+        for directory in directories:
+            if os.path.isdir(directory):
+                for file_name in os.listdir(directory):
+                    file_path = os.path.join(directory, file_name)
+                    if os.path.isfile(file_path):  # Ensure it's a file
+                        file_set.add(file_path)
+        return list(file_set)
+    
+    train_input_files = get_files_from_directories(input_directories)
+    train_label_files = get_files_from_directories(label_directories)
+    
+    return train_input_files, train_label_files
+
+
 def create_dataset(val_ratio=0.1, test_ratio=0.1, input_dir = './preprocessed_data3d/input_data', label_dir = './preprocessed_data3d/labels', batch_size = 16):
 	if val_ratio + test_ratio >= 1:
 		raise Exception("split exceeds 100%")
@@ -40,9 +59,18 @@ def create_dataset(val_ratio=0.1, test_ratio=0.1, input_dir = './preprocessed_da
 	paired_filenames = list(zip(input_filenames, label_filenames))
 
 	# get the split
-	(train_input, train_label), (validate_input, validate_output), (test_input, test_output) = split_filenames(paired_filenames, val_ratio, test_ratio)
+	train, validate, test = split_filenames(paired_filenames, val_ratio, test_ratio)
+	(train_input_directories, train_label_directories) = train
+	(validate_input_directories, validate_labels_directories) = validate
+	(test_input_directories, test_labels_directories) = test
 
-	
+	# get list of training data from directory lists 
+	(train_input, train_label) = concatenate_directory_entries(train_input_directories, train_label_directories)
+	(validate_input, validate_label) = concatenate_directory_entries(validate_input_directories, validate_labels_directories)
+	(test_input, test_label) = concatenate_directory_entries(test_input_directories, test_labels_directories)
+
+
+
 
 
 # Example usage:
