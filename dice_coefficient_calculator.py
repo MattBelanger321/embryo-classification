ca719419b293e20fe285abf134a82d100bf4bdf5
Matthew Belanger <matt_belanger1@outlook.com>
2024-11-05 10:37:27 -0500

    removed redudant code

--- dice_coefficient_calculator.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras import backend as K
-
-# Compute dice coefficient
-def compute_dice_coefficient(y_true, y_pred, smooth=1):
-    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
-    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
-    dice_coefficient = K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
-    return dice_coefficient
