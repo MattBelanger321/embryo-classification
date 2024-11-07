184fd8582f817a5d4709ade37cba3b1b20b1db1a
Matthew Belanger <matt_belanger1@outlook.com>
2024-11-06 21:33:35 -0500

    made unet2d its own file

--- unet/2d/train_unet.py
+++ unet/2d/train_unet.py
@@ -10,12 +10,12 @@ sys.path.insert(0, parent_dir)
 import tensorflow as tf
 import glob
 import random
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, concatenate, Input
+
 from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.optimizers import SGD, Adam
 import parse_training_csv as parser
 from batch_generator import UNetBatchGenerator as batch_generator
+
+from unet_2d import define_unet
 
 from sklearn.model_selection import train_test_split
 
@@ -24,68 +24,6 @@ import FlushableStream
 import numpy as np
 import cv2
 import metrics_calculator as mc
-
-def define_unet():
-    inputs = Input(shape=(256, 256, 1))  # Adjust input shape as needed
-
-	# we use padding same so that the input doesnt change during convolutions
-	# the naming convention is
-    # c for convolution
-    # d for down sampling
-    # u for up sammpling
-    # the numbers correspond to the position of the group in the U,
-    # see section 2 of the UNet paper and figure 1
-
-    # Encoder (downsampling)
-    c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(inputs)
-    c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c1)
-    d1 = MaxPooling2D((2, 2))(c1)
-
-    c2 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d1)
-    c2 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c2)
-    d2 = MaxPooling2D((2, 2))(c2)
-
-    c3 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d2)
-    c3 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c3)
-    d3 = MaxPooling2D((2, 2))(c3)
-
-    c4 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d3)
-    c4 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c4)
-    d4 = MaxPooling2D((2, 2))(c4)
-
-    # Bottleneck, this is the bottom of the U
-    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d4)
-    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c5)
-
-    # Decoder (upsampling)
-    u6 = UpSampling2D((2, 2))(c5)
-    u6 = concatenate([u6, c4])  # Skip connection
-    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u6)
-    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c6)
-
-    u7 = UpSampling2D((2, 2))(c6)
-    u7 = concatenate([u7, c3])  # Skip connection
-    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u7)
-    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c7)
-
-    u8 = UpSampling2D((2, 2))(c7)
-    u8 = concatenate([u8, c2])  # Skip connection
-    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u8)
-    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c8)
-
-    u9 = UpSampling2D((2, 2))(c8)
-    u9 = concatenate([u9, c1])  # Skip connection
-    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u9)
-    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c9)
-
-    outputs = Conv2D(3, (1, 1), activation='sigmoid')(c9)  # Use softmax for multi-class segmentation
-
-    # Compile the model
-    model = Model(inputs=[inputs], outputs=[outputs])
-    opt = Adam( clipnorm=1.0)  # Norm is clipped to 1.0
-    model.compile(optimizer=opt, loss='bce', metrics=[mc.accuracy_metric])
-    
-    return model
 
 def train_unet(train, test, model, batch_size=32, epochs=1, spe=1, vsteps=1, save_path="model_epoch_{epoch:02d}.keras"):
     print("Fitting...")
---
+++ unet/2d/unet_2d.py
@@ -0,0 +1,65 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, concatenate, Input
+from tensorflow.keras.optimizers import SGD, Adam
+
+def define_unet():
+    inputs = Input(shape=(256, 256, 1))  # Adjust input shape as needed
+
+	# we use padding same so that the input doesnt change during convolutions
+	# the naming convention is
+    # c for convolution
+    # d for down sampling
+    # u for up sammpling
+    # the numbers correspond to the position of the group in the U,
+    # see section 2 of the UNet paper and figure 1
+
+    # Encoder (downsampling)
+    c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(inputs)
+    c1 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c1)
+    d1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d1)
+    c2 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c2)
+    d2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d2)
+    c3 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c3)
+    d3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d3)
+    c4 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c4)
+    d4 = MaxPooling2D((2, 2))(c4)
+
+    # Bottleneck, this is the bottom of the U
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(d4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c5)
+
+    # Decoder (upsampling)
+    u6 = UpSampling2D((2, 2))(c5)
+    u6 = concatenate([u6, c4])  # Skip connection
+    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u6)
+    c6 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c6)
+
+    u7 = UpSampling2D((2, 2))(c6)
+    u7 = concatenate([u7, c3])  # Skip connection
+    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u7)
+    c7 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c7)
+
+    u8 = UpSampling2D((2, 2))(c7)
+    u8 = concatenate([u8, c2])  # Skip connection
+    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u8)
+    c8 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c8)
+
+    u9 = UpSampling2D((2, 2))(c8)
+    u9 = concatenate([u9, c1])  # Skip connection
+    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u9)
+    c9 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c9)
+
+    outputs = Conv2D(3, (1, 1), activation='sigmoid')(c9)  # Use softmax for multi-class segmentation
+
+    # Compile the model
+    model = Model(inputs=[inputs], outputs=[outputs])
+    opt = Adam( clipnorm=1.0)  # Norm is clipped to 1.0
+    model.compile(optimizer=opt, loss='bce', metrics=["accuracy"])
+    
+    return model
